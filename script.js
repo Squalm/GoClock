ccf329c6a4600e55bcf77777f3be1936022c719e
Squalm <43246103+Squalm@users.noreply.github.com>
2021-07-29 16:17:34 +0100

    attempt to add settings #1

--- script.js
+++ script.js
@@ -351,3 +351,19 @@ function formatTimer() {
 
 }
 
+// Show and hide the settings menu on button pressed.
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("onClick", function() {
+
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+
+    });
+}
